c73e34d15ae847c8fe7303b6ddd87951f824a077
Neil Ramchandani <neilramch@Neils-MacBook-Air.local>
2025-01-01 20:53:52 -0500

    title

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,27 @@
-import Header from './components/Header'
-import Profile from './components/Profile'
-import Projects from './components/Projects'
-
+import './App.css';
+import Header from './components/Header';
+import Profile from './components/Profile';
+import Projects from './components/Projects';
+import Contact from './components/Contact';
 
 
 function App() {
- 
-
   return (
-    <div className='App'>
-    
-      <Header></Header>
-      <Profile></Profile>
-      <Projects></Projects>
+    <div className="app">
+      <section>
+        <Header />
+      </section>
+      <section>
+        <Profile />
+      </section>
+      <section>
+        <Projects />
+      </section>
+      <section>
+        <Contact />
+      </section>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Contact.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './Contact.css'
+
+function Contact() {
+  return (
+    <div>
+            <div className='contact-details'>
+                <ul>
+                    <h3>Contact Details</h3>
+                    <div className='underline'></div>
+                    <li>Email: neilramch@gmail.com</li>
+                </ul>
+            </div>
+      
+    </div>
+  )
+}
+
+export default Contact
